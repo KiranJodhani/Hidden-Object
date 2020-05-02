867e4b68e04049edae9db3ca628780cb192ba2d3
Kiran Jodhani <kiran.jodhani.w@gmail.com>
2020-05-02 18:13:54 +0530

    Hidden-Object

---
+++ Assets/HOGAssets/CS_Assets/CS_Scripts/Instruction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject InstructionScreen;
+    void Start()
+    {
+		RevenueManager.Instance.ShowInterstitialAd ();
+		Time.timeScale = 0;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void HideInstructionPanel()
+	{
+		Time.timeScale = 1;
+		InstructionScreen.SetActive (false);
+	}
+}
